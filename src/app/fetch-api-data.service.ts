c44f72eef2dd9b816643d049f259d638ca5eb263
illia1008 <158865982+illia1008@users.noreply.github.com>
2024-11-08 11:01:53 -0500

    Signup/Register

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,11 +5,11 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://myflix-myapp-e7d3dd6fff4f.herokuapp.com';
+const apiUrl = 'https://myflix-myapp-e7d3dd6fff4f.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
